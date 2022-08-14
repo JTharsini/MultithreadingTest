f2df9f28b64a62871467acebb0c9118e84d37b5f
JJeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2022-08-14 14:13:01 +0530

    Demonstrate JVM exits if only daemon thread is remaining

---
+++ src/main/java/JVMExitsIfItsTheOnlyThread.java
@@ -0,0 +1,29 @@
+public class JVMExitsIfItsTheOnlyThread
+{
+  public static void main(String[] args)
+  {
+    Runnable runnable = () -> {
+      while (true)
+      {
+        sleep(1000);
+        System.out.println("Running");
+      }
+    };
+    Thread thread = new Thread(runnable);
+    thread.setDaemon(true);
+    thread.start();
+    sleep(3000);
+  }
+
+  private static void sleep(long millis)
+  {
+    try
+    {
+      Thread.sleep(millis);
+    }
+    catch (InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
