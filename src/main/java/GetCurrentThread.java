4899af008601eb580259eb62dc9f10dad76d0194
JJeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2022-08-14 13:00:11 +0530

    Get current thread

---
+++ src/main/java/GetCurrentThread.java
@@ -0,0 +1,11 @@
+public class GetCurrentThread
+{
+  public static void main(String[] args)
+  {
+    Thread thread = new Thread(() -> {
+      String threadName = Thread.currentThread().getName();
+      System.out.println(threadName);
+    });
+    thread.start();
+  }
+}
