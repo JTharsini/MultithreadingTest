fdfceafd6f7337f3294ea4927d20d26e9026b966
JJeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2022-08-14 13:07:08 +0530

    add another thread

--- src/main/java/GetCurrentThread.java
+++ src/main/java/GetCurrentThread.java
@@ -2,10 +2,13 @@ public class GetCurrentThread
 {
   public static void main(String[] args)
   {
-    Thread thread = new Thread(() -> {
+    Runnable runnable = () -> {
       String threadName = Thread.currentThread().getName();
       System.out.println(threadName);
-    }, "JJThread");
+    };
+    Thread thread = new Thread(runnable, "JJThread");
+    Thread thread2 = new Thread(runnable, "JJThread2");
     thread.start();
+    thread2.start();
   }
 }
